41517fd483ebd75a04341572d9e76dfb848c87bc
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-04-25 21:10:55 +0530

    feat: initial commit

---
+++ memer.ai/memer_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  memer_aiApp.swift
+//  memer.ai
+//
+//  Created by Prashanna Rajbhandari on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct memer_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
